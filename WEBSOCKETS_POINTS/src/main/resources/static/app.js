9493b7db160ca06aae0142025f011b0ca2a68da7
Diego Puerto <diego23p@hotmail.com>
2020-03-17 21:25:56 -0500

    PARTE 3

--- WEBSOCKETS_POINTS/src/main/resources/static/app.js
+++ WEBSOCKETS_POINTS/src/main/resources/static/app.js
@@ -7,6 +7,8 @@ var app = (function () {
         }        
     }
     
+	var _num = 0;
+	
     var stompClient = null;
 
     var addPointToCanvas = function (point) {        
@@ -33,10 +35,10 @@ var app = (function () {
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
         
-        //subscribe to /topic/newpoint when connections succeed
+        //subscribe to /topic/newpoint+num when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+            stompClient.subscribe('/topic/newpoint'+_num, function (eventbody) {
                 var theObject = JSON.parse(eventbody.body);
 				//callback("New Point: " + theObject.x + " " + theObject.y);
 				callback(new Point(theObject.x,theObject.y));
@@ -66,7 +68,7 @@ var app = (function () {
             //addPointToCanvas(pt);
 
             //publicar el evento
-			stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
+			stompClient.send("/topic/newpoint"+_num, {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
@@ -75,7 +77,19 @@ var app = (function () {
             }
             setConnected(false);
             console.log("Disconnected");
-        }
+        },
+		
+		setNumDibujo: function() {
+			var num = prompt("Ingrese el número de su dibujo: ", "");
+			if (num == null){
+				num = 0;
+				alert("Número incorrecto, se le asignó el 0");
+			}
+			else{
+				_num = num;
+				document.getElementById("dibujoNum").innerHTML = _num ;
+			}
+		}
     };
 
 })();
